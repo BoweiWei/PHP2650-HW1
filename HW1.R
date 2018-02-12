dfe45dc3cabd5d8576626cc4d202036e0e07c28a
BoweiWei <bowei_wei@brown.edu>
2018-02-11 20:12:27 -0500

    Question 2 finished, csv file created

--- HW1.R
+++ HW1.R
@@ -32,7 +32,7 @@ for (s in 1:length(symSP)) {
 }
 
 ## Question 2
-df2 <- read.csv("~/Documents/PHP2650-HW1/NIHHarvard.csv", header = TRUE, sep = ",")
+df2 <- read.csv("NIHHarvard.csv", header = TRUE, sep = ",")
 for (i in 1: nrow(df2)) {
   filter(substring(as.character(df2$Activity[i]),1,1) = c("T", "F")) {
     df2 <- df2[-i]
@@ -45,41 +45,47 @@ df2 %>%
 df3 <- as.data.frame(df3)
 
 namelist <- unique(df3$Contact.PI...Project.Leader)
-namelist <- word(namelist, 1,2, sep=" ")
-namelist <- lapply(namelist, str_replace_all, fixed(","), "+")
-namelist <- lapply(namelist, str_replace_all, fixed(" "), "")
+namelist.n <- lapply(namelist, str_replace_all, fixed("   "), "   ")
+#namelist.exp <- as.data.frame(namelist, stringsAsFactors = FALSE)
+#namelist.exp <- droplevels(namelist.exp)
+namelist.s <- word(namelist, 1,2, sep=" ")
+namelist.s <- lapply(namelist.s, str_replace_all, fixed(","), "+")
+namelist.s <- lapply(namelist.s, str_replace_all, fixed(" "), "")
+
 
 ## approaches 
 library(xml2)                           # another XML package
 library(httr)                           # another one for getting web data with advanced features 
 #html <- htmlParse( content( GET(url) , as="parsed")  )
 
-getGS <- function(aname, affli) {
+getGS <- function(aname, affli, bname) {
   re <- NULL
   topicname <- paste0(aname, "[Author]", affli, "[Affiliation]") 
       url <- paste0("https://www.ncbi.nlm.nih.gov/pubmed/?term=", topicname)
-    #url <- getURL(url)
     html <- htmlParse(getURL(url), encoding="UTF-8")
     anum <- xpathSApply(html, "//*[@class='title_and_pager']", xmlValue)
     anum <- toString(anum)
     anum <- str_replace_all(anum, fixed("  "), "")
     if (str_count(anum, "\\S+") > 4) {
     anum <- gsub("[^0-9\\.]", "", word(anum,7)) 
-    re <- c(aname, anum)
+    re <- c("searchname" = aname, "Number of Publications" = anum, "Contact.PI...Project.Leader" = bname)
     } else {
       anum <- gsub("[^0-9\\.]", "", word(anum,3))
-      re <- c(aname, anum)
+      re <- c("searchname" = aname, "Number of Publications" = anum, "Contact.PI...Project.Leader" = bname)
     }
     #Sys.sleep(10+runif(1)*20)       #random pause to fake human clicking or downloading
   return(re)
 }
 
 ## loop 
-re <-  lapply(namelist, function(x) getGS(x, "harvard") )
+re <- mapply(function(x, y) {getGS(x, "Harvard", y)}, x = namelist.s, y = namelist.n)
 
+## join the data frame
+re.new <- as.data.frame(re, stringsAsFactors = FALSE)
+re.new <- as.data.frame(t(re.new))
+final <- left_join(df2, re.new, by = "Contact.PI...Project.Leader")
+final[, "searchname"] <- list(NULL)
 
-a <- htmlParse(getURL("https://www.ncbi.nlm.nih.gov/pubmed/?term=ESSEX+MYRON[Author]harvard[affiliation]"), encoding="UTF-8")
-b <- xpathSApply(a, "//*[@class='title_and_pager']", xmlValue)
-c <- str_replace_all(b, fixed("  "), "")
-str_count(c, "\\S+")
-word(c,4)
+## export the csv file from data frame
+write.csv(final, "Mod.NIHHarvard.csv")
+
